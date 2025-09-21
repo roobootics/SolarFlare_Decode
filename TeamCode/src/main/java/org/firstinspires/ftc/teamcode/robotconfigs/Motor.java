d92e78605228b0f137e839cb0f17dca14c4e607a
roobootics <arick@solarflarerobotics.org>
2025-09-21 14:44:45 -0400

    functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotconfigs/Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotconfigs/Motor.java
@@ -13,12 +13,19 @@ public class Motor implements RobotConfig {
         motor = new BotMotor(
                 "motor", List.of(new DcMotorExData("motor")),
                 ()->2000.0,()->0.0,20.0,5.0,
-                new String[]{"PID","setVelocity"},
+                new String[]{"PID","setVelocity","VelocityPIDF"},
                 new ControlSystem<>(new TrapezoidalMotionProfile<>(10000,1000),new PositionPID<>(0.015,0.00001,0.0001)),
                 new ControlSystem<>(
                         new String[]{"targetVelocity"},
                         List.of((BotMotor motor) -> targetVelocity),
-                        new TrapezoidalMotionProfile<>(10000,1000),new PositionPID<>(0.015,0.00001,0.0001)
+                        (String name, Double vel)->motor.setVelocity(vel,name),
+                        new SetVelocity()
+                ),
+                new ControlSystem<>(
+                        new String[]{"targetVelocity"},
+                        List.of((BotMotor motor) -> targetVelocity),
+                        new BasicFeedforward<>(new double[]{0.000357185},new String[]{"targetVelocity"}),
+                        new VelocityPID<>(0.0001,0,0)
                 )
         );
     }
