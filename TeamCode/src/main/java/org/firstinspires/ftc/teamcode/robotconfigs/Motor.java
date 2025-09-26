324bd34f7bc969ad4a5ffbd1c3a4258843de3ae4
roobootics <arick@solarflarerobotics.org>
2025-09-25 20:18:02 -0400

    cleaning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotconfigs/Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotconfigs/Motor.java
@@ -9,26 +9,25 @@ import java.util.List;
 
 public class Motor implements RobotConfig {
     public static BotMotor motor;
-    public static TrapezoidalMotionProfile<BotMotor> profile;
+    public static TrapezoidalMotionProfile profile;
     public static double targetVelocity;
     @Override
     public void init() {
-        profile=new TrapezoidalMotionProfile<>(10000,1000);
+        profile=new TrapezoidalMotionProfile(10000,1000);
         motor = new BotMotor(
-                "motor", DcMotorSimple.Direction.FORWARD,
-                ()->2000.0,()->0.0,20.0,5.0,
+                "motor", DcMotorSimple.Direction.FORWARD, 20.0,5.0,
                 new String[]{"PID","setVelocity","VelocityPIDF"},
-                new ControlSystem<>(profile,new PositionPID<>(0.015,0.0,0.0)),
+                new ControlSystem<>(profile,new PositionPID(0.015,0.0,0.0)),
                 new ControlSystem<>(
                         new String[]{"targetVelocity"},
-                        List.of(() -> targetVelocity),
+                        List.of(()->targetVelocity),
                         (Double vel)->motor.setVelocity(vel),
                         new SetVelocity()
                 ),
                 new ControlSystem<>(
                         new String[]{"targetVelocity"},
-                        List.of(() -> targetVelocity),
-                        new BasicFeedforward<>(new double[]{0.000357185},new String[]{"targetVelocity"}),
+                        List.of(()->targetVelocity),
+                        new BasicFeedforward(new double[]{0.000357185},new String[]{"targetVelocity"}),
                         new VelocityPID(0.0001,0,0)
                 )
         );
