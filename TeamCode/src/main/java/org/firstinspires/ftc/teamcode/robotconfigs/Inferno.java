9c3b0380a9505d5fe91b5f1f3594bdb74d7477e5
roobootics <arick@solarflarerobotics.org>
2025-10-26 20:22:31 -0400

    configuring robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotconfigs/Inferno.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotconfigs/Inferno.java
@@ -1,24 +1,16 @@
 package org.firstinspires.ftc.teamcode.robotconfigs;
 
-import android.graphics.Bitmap;
-
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.function.Consumer;
-import org.firstinspires.ftc.robotcore.external.function.Continuation;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.CameraControl;
+
 import org.firstinspires.ftc.teamcode.base.Components;
 import org.firstinspires.ftc.teamcode.base.Components.*;
 import org.firstinspires.ftc.teamcode.pedroPathing.Pedro;
 import org.firstinspires.ftc.teamcode.presets.PresetControl.*;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.VisionProcessor;
 
 import java.util.List;
 
@@ -33,9 +25,16 @@ public class Inferno implements RobotConfig{
     public static SyncedActuators<BotServo> turretPitch;
     public static BotMotor frontIntake;
     public static BotMotor backIntake;
+    public static BotServo frontIntakeGate;
+    public static BotServo backIntakeGate;
     public static RevColorSensorV3[] sensors = new RevColorSensorV3[3];
     public static Limelight3A limelight;
-    public static VisionPortal aprilTagCam1;
+    public static BotServo limelightPitch;
+    public enum Color{
+        BLUE,
+        GREEN,
+    }
+    public static Color[] ballStorage = new Color[3];
     @Override
     public void init() {
         Pedro.createFollower(new Pose(0,0,0));
@@ -56,25 +55,29 @@ public class Inferno implements RobotConfig{
                 new BotServo("turretPitchRight",Servo.Direction.REVERSE,422,5,270,90)
         );
         turretYaw = new SyncedActuators<>(
-                new BotServo("turretYawLeft",Servo.Direction.FORWARD,422,5,270,90),
-                new BotServo("turretYawRight",Servo.Direction.FORWARD,422,5,270,90)
+                new BotServo("turretYawLeft",Servo.Direction.FORWARD,422,5,355,90),
+                new BotServo("turretYawRight",Servo.Direction.FORWARD,422,5,355,90)
         );
+        turretYaw.call((BotServo servo)->servo.setTargetBounds(()->355.0,()->0.0));
         frontIntake = new BotMotor("frontIntake",DcMotorSimple.Direction.FORWARD);
         backIntake = new BotMotor("backIntake",DcMotorSimple.Direction.REVERSE);
         frontIntake.setKeyPowers(
-                new String[]{"thisSideIntake","transfer","otherSideIntake","expel"},
+                new String[]{"intake","thisSideTransfer","otherSideTransfer","stopped","expel"},
                 new double[]{}
         );
         backIntake.setKeyPowers(
-                new String[]{"thisSideIntake","transfer","otherSideIntake","expel"},
+                new String[]{"intake","thisSideTransfer","otherSideTransfer","stopped","expel"},
                 new double[]{}
         );
+        frontIntakeGate = new BotServo("frontIntakeGate", Servo.Direction.FORWARD,422,5,270,90);
+        backIntakeGate = new BotServo("backIntakeGate", Servo.Direction.REVERSE,422,5,270,90);
+        frontIntakeGate.setKeyPositions(new String[]{"open","closed"},new double[]{});
+        backIntakeGate.setKeyPositions(new String[]{"open","closed"},new double[]{});
         sensors[0] = Components.getHardwareMap().get(RevColorSensorV3.class,"sensor1");
         sensors[1] = Components.getHardwareMap().get(RevColorSensorV3.class,"sensor2");
         sensors[2] = Components.getHardwareMap().get(RevColorSensorV3.class,"sensor3");
         limelight=Components.getHardwareMap().get(Limelight3A.class,"limelight");
-        aprilTagCam1 = new VisionPortal.Builder()
-                .setCamera(Components.getHardwareMap().get(CameraName.class,"aprilTagCam1"))
-                .build();
+        limelightPitch = new BotServo("limelightPitch",Servo.Direction.FORWARD,422,5,270,90);
+        limelightPitch.setKeyPositions(new String[]{"balls","apriltag","obelisk"},new double[]{});
     }
 }
