fb5c053ae7235462c5f73667ba5234774c4ce0ef
roobootics <arick@solarflarerobotics.org>
2026-02-26 21:53:48 -0500

    calibrate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotconfigs/Fisiks.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotconfigs/Fisiks.java
@@ -22,7 +22,7 @@ public abstract class Fisiks {
     final static double BALL_RAD = 2.5;
     final static double SURFACE_SPEED_RATIO = 0.75;
 
-    final static double FRICTION = 0.5;
+    final static double FRICTION = 0.9;
     final static double AUTHORITY = 0.25;
     final static double TRANSLATIONAL_DRAG = 0.50;
     final static double ANGULAR_DRAG = 0;
